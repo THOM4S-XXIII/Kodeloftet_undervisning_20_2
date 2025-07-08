9782a302c0dfb087191aa7db763348e2cf876d98
THOM4S-XXIII <thomas-s93@hotmail.com>
2025-07-08 11:05:54 +0200

    async function

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ sunIcon.addEventListener("click", function () {
   body.classList.remove("light");
   body.classList.add("dark");
 });
+
+
+// 
+// 
+
+// Async function
+
+setTimeout(function () { 
+  console.log("Hei")
+}, 3000); //Millisekund
+
+console.log("Venter på svar..");
+
+// 
+// const fruitBowl = ["Epler", "Bananer", "Pærer"];
+
+async function fetchingData() { 
+  const data = await fetch("data.json");
+  console.log(data);
+
+const response = await data.json();
+console.log(response[1]);
+
+const fruitBowl = response.join(",");
+console.log(fruitBowl);
+}
+
+// fetchingData();
+setTimeout(fetchingData, 3000);
